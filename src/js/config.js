c6a04a9bb7c9c8f3709eafba80aae1d32e2f8c01
Wabbito1 <sapunaru.teodor1@gmail.com>
2022-03-09 18:25:59 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "91433901-7a6b-4fa5-881f-482d6d78e63f";
+export const MODAL_CLOSE_SEC = 2.5;
