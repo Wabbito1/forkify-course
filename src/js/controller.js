b8cbec03acab9770cb4bc58403d078d057e3653e
Wabbito1 <sapunaru.teodor1@gmail.com>
2022-03-09 18:42:42 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -141,6 +141,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the application!");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -149,7 +153,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome!");
+  newFeature();
 };
 
 // clearBookmarks();
